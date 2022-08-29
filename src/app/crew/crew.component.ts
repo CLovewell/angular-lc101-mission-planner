3f08e64702f1ee02d3a0d6190490fd0d5584af90
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-08-29 16:47:24 -0500

    Completed Angular studio 2 w/o bonus missions.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,14 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true }
   ];
+  memberBeingEdited: object = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(name: string, firstMission: boolean): void {
+    this.crew.push(
+      {
+        name: name,
+        firstMission: firstMission
+      }
+    );
+  }
+
+  remove(member: object): void {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object): void {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object): void {
+    member["name"] = name;
+    this.memberBeingEdited = null;
   }
 
 }
